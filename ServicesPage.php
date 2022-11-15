7daaec7663f78512d4ddb52e9df24090d6db2693
AzureJames <azurethegreat@gmail.com>
2022-11-14 21:51:29 -0700

    services draft

--- ServicesPage.php
+++ ServicesPage.php
@@ -7,29 +7,43 @@ get_header();
 <div class="testing">
 
     <h1>SERVICES</h1>
-    <p>Whaddya Need?</p>
+    <p class="whaddya">Whaddya Need?</p>
 
-
-    <div>
-        <h2>Cutting</h2>
-        <p>Custom Clipper - $<?php the_field('custom_clipper'); ?> For current clients only, I'll recommend this service if your haircut is quick or short.</p>
-        <p>Signature Scissor - $<?php the_field('signature_clipper'); ?> For new guests, guests with long or thick hair, or if you are looking for a new style.</p>
+    <div class="card">
+        <div class="card-top">
+            <h2>Cutting</h2>
+        </div>    
+        <img src="http://placekitten.com/100/100" alt="">
+        <ul>
+            <li><p>Custom Clipper - $<span><?php the_field('custom_clipper'); ?></span> For current clients only, I'll recommend this service if your haircut is quick or short.</p></li>
+            <li><p>Signature Scissor - $<span><?php the_field('signature_clipper'); ?></span> For new guests, guests with long or thick hair, or if you are looking for a new style.</p></li>
+        </ul>
     </div>
 
-    <div>
+    <div class="card">
+    <div class="card-top">
         <h2>Coloring</h2>
-        <p>Solid Color - $<?php the_field('solid_color'); ?>+ Color is applied from root to ends to achieve a glossy, fresh result.</p>
-        <p>Regrowth Touch Up - $<?php the_field('regrowth_touch_up'); ?> Color is applied to the roots only.</p>
-        <p>Highlights/Lowlights (Partial) - $<?php the_field('highlights_partial'); ?>+ Color or lightener is applied in foils around the face and crown areas.</p>
-        <p>Highlights/Lowlights (Full) - $<?php the_field('highlights_full'); ?>+ Color or lightener is applied in foils throughout your hair for maximum effect.</p>
-        <p>Hair Painting/ Balayage - $<?php the_field('hair_painting'); ?>+ Service encompasses techniques and looks including balayage. ombre, or foilayage.</p>
-        <p>Color Transformation - $<?php the_field('color_transformation'); ?>+ service is inclusive of any treatments or product. This is ideal for the guest that wants a big change or something creative and bright.</p>
+    </div>    
+        <img src="http://placekitten.com/100/100" alt="">
+        <ul>
+            <li><p>Solid Color - $<span><?php the_field('solid_color'); ?></span>+ Color is applied from root to ends to achieve a glossy, fresh result.</p></li>
+            <li><p>Regrowth Touch Up - $<span><?php the_field('regrowth_touch_up'); ?></span> Color is applied to the roots only.</p></li>
+            <li><p>Highlights/Lowlights (Partial) - $<span><?php the_field('highlights_partial'); ?></span>+ Color or lightener is applied in foils around the face and crown areas.</p></li>
+            <li><p>Highlights/Lowlights (Full) - $<span><?php the_field('highlights_full'); ?></span>+ Color or lightener is applied in foils throughout your hair for maximum effect.</p></li>
+            <li><p>Hair Painting/ Balayage - $<span><?php the_field('hair_painting'); ?></span>+ Service encompasses techniques and looks including balayage. ombre, or foilayage.</p></li>
+            <li><p>Color Transformation - $<span><?php the_field('color_transformation'); ?></span>+ service is inclusive of any treatments or product. This is ideal for the guest that wants a big change or something creative and bright.</p></li>
+        </ul>
     </div>
 
-    <div>
-        <h2>Extension</h2>
-        <p>A consultation is required to ensure color matching and an appropriate maintenance schedule that will fit your lifestyle. </p>
-        <p>A 50% deposit is required to order the extensions once the consultation is complete.</p>
+    <div class="card">
+        <div class="card-top">
+            <h2>Extension</h2>
+        </div>    
+        <img src="http://placekitten.com/100/100" alt="">
+        <ul>
+            <li><p>A consultation is required to ensure color matching and an appropriate maintenance schedule that will fit your lifestyle. </p></li>
+            <li><p>A 50% deposit is required to order the extensions once the consultation is complete.</p></li>
+        </ul>
     </div>
 
 
