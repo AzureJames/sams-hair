d0ec0c8d0a08625fce6581dfe5e92a430b4eed6a
Justin Shaw <shaw.justin@shaw.ca>
2022-11-15 14:03:28 -0700

    Adding Contact Form

--- contactformpage.php
+++ contactformpage.php
@@ -5,12 +5,14 @@ get_header();
 ?>
 
 <div class="testing">
-    <h1>Copntact Form Page</h1>
+    <h1>CONTACT ME</h1>
 
-    <h2>STILL WORKING ON THIS PAGE GIVE ME TIME - JUSTIN</h2>
+    <?php echo do_shortcode('[contact-form-7 id="89" title="Sams Contact form"]'); ?>
 </div>
 
 
 <?php
 
 get_footer();
+
+?>
