54ff861e6a98d664890270e9be4a257914e4d6a6
Justins45 <shaw.justin@shaw.ca>
2022-11-17 13:20:56 -0700

    Completed Home Page All views
    Completed Home Page All views

--- homepage.php
+++ homepage.php
@@ -3,10 +3,10 @@
 
 get_header();
 ?>
-<div class="home">
+<div class="home-page">
     <div class="home-head">
         <div class="banner-h1-bg">
-            <h1>HAIR BY SAM KWETIO</h1>
+            <h1>HAIR BY <br>SAM KWETIO</h1>
             <p>Proud to offer many service combinations and options, this is a sample of what is offered in salon. These services and prices are for existing clients. If you're a new guest, definitely check out the New Guest Page for helpful details.</p>
         </div>
         <div class="home-banner">
@@ -20,19 +20,21 @@ get_header();
 
     <div class="most-pop">
         <h2>Most Popular Services</h2>
+        <div class="home-flex">
+            <div class="pop-card">
+                <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-cuts-e1668644382959.jpg" alt="Image of golden hair stylist equipment.">
+                <p>Get a Custom Clipper cut for a quick or short haircut when you need to freshen up, or go for a Signature Scissor cut for long or thick hair, or you're looking for something new. </p>
+            </div>
+            <div class="pop-card">
+                <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-color-e1668644628593.jpg" alt="Image of clients new hair color.">
+                <p>Add some colour to your style. I do everything from solid colours to complete total transformations and touchups.</p>
+            </div>
+            <div class="pop-card">
+                <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-extensions-e1668644796694.jpg" alt="two ladies hugging on sidewalk in the sun.">
+                <p>Get the longer hair you always wanted without waiting to grow it all out. Add extensions today!</p>
+            </div>
+        </div>
 
-        <div class="pop-card">
-            <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-cuts-e1668644382959.jpg" alt="Image of golden hair stylist equipment.">
-            <p>Get a Custom Clipper cut for a quick or short haircut when you need to freshen up, or go for a Signature Scissor cut for long or thick hair, or you're looking for something new. </p>
-        </div>
-        <div class="pop-card">
-            <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-color-e1668644628593.jpg" alt="Image of clients new hair color.">
-            <p>Add some colour to your style. I do everything from solid colours to complete total transformations and touchups.</p>
-        </div>
-        <div class="pop-card">
-            <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-extensions-e1668644796694.jpg" alt="two ladies hugging on sidewalk in the sun.">
-            <p>Get the longer hair you always wanted without waiting to grow it all out. Add extensions today!</p>
-        </div>
 
         <div class="contact-btn">
             <a href="contact" class="contact-me-btn">Contact Me</a>
