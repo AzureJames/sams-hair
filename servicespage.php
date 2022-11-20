04202c187b991d46211c9ddcdc4c8310ecc812c1
Justins45 <shaw.justin@shaw.ca>
2022-11-20 13:49:20 -0700

    Alterd services a little / Added max width to about page
    Alterd services a little / Added max width to about page

--- servicespage.php
+++ servicespage.php
@@ -15,53 +15,49 @@ get_header();
             <div class="card-top">
                 <h2 class="h2one">Cutting</h2>
             </div>
-            <img src="http://placekitten.com/150/150" alt="">
         </div>
-        <ul>
-            <li>
-                <p><span>Custom Clipper - $<?php the_field('custom_clipper'); ?></span> For current clients only, I'll recommend this service if your haircut is quick or short.</p>
-            </li>
-            <li>
-                <p><span>Signature Scissor - $<?php the_field('signature_clipper'); ?></span> For new guests, guests with long or thick hair, or if you are looking for a new style.</p>
-            </li>
 
-
-        </ul>
+        <div class="service-card">
+            <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-cuts-e1668644382959.jpg" alt="Image of golden hair stylist equipment.">
+            <ul>
+                <li>
+                    <p><span>Custom Clipper - $<?php the_field('custom_clipper'); ?></span> For current clients only, I'll recommend this service if your haircut is quick or short.</p>
+                </li>
+                <li>
+                    <p><span>Signature Scissor - $<?php the_field('signature_clipper'); ?></span> For new guests, guests with long or thick hair, or if you are looking for a new style.</p>
+                </li>
+            </ul>
+        </div>
     </div>
-
-
-
 
     <div class="card">
         <div class="card-top">
             <h2 class="h2two">Coloring</h2>
-        </div> 
-        <div class="inner-card">
-            <div class="sml-col">
-                <img src="http://placekitten.com/150/150" alt="">
-            </div>
-            <img src="http://placekitten.com/150/150" alt="">
         </div>
-        <ul>
-            <li>
-                <p><span>Solid Color - $<?php the_field('solid_color'); ?></span>+ Color is applied from root to ends to achieve a glossy, fresh result.</p>
-            </li>
-            <li>
-                <p><span>Regrowth Touch Up - $<?php the_field('regrowth_touch_up'); ?></span> Color is applied to the roots only.</p>
-            </li>
-            <li>
-                <p><span>Highlights/Lowlights (Partial) - $<?php the_field('highlights_partial'); ?></span>+ Color or lightener is applied in foils around the face and crown areas.</p>
-            </li>
-            <li>
-                <p><span>Highlights/Lowlights (Full) - $<?php the_field('highlights_full'); ?></span>+ Color or lightener is applied in foils throughout your hair for maximum effect.</p>
-            </li>
-            <li>
-                <p><span>Hair Painting/ Balayage - $<?php the_field('hair_painting'); ?></span>+ Service encompasses techniques and looks including balayage. ombre, or foilayage.</p>
-            </li>
-            <li>
-                <p><span>Color Transformation - $<?php the_field('color_transformation'); ?></span>+ service is inclusive of any treatments or product. This is ideal for the guest that wants a big change or something creative and bright.</p>
-            </li>
-        </ul>
+        <div class="service-card reverse">
+            <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-color-e1668644628593.jpg" alt="Image of clients new hair color.">
+            <ul>
+                <li>
+                    <p><span>Solid Color - $<?php the_field('solid_color'); ?></span>+ Color is applied from root to ends to achieve a glossy, fresh result.</p>
+                </li>
+                <li>
+                    <p><span>Regrowth Touch Up - $<?php the_field('regrowth_touch_up'); ?></span> Color is applied to the roots only.</p>
+                </li>
+                <li>
+                    <p><span>Highlights/Lowlights (Partial) - $<?php the_field('highlights_partial'); ?></span>+ Color or lightener is applied in foils around the face and crown areas.</p>
+                </li>
+                <li>
+                    <p><span>Highlights/Lowlights (Full) - $<?php the_field('highlights_full'); ?></span>+ Color or lightener is applied in foils throughout your hair for maximum effect.</p>
+                </li>
+                <li>
+                    <p><span>Hair Painting/ Balayage - $<?php the_field('hair_painting'); ?></span>+ Service encompasses techniques and looks including balayage. ombre, or foilayage.</p>
+                </li>
+                <li>
+                    <p><span>Color Transformation - $<?php the_field('color_transformation'); ?></span>+ service is inclusive of any treatments or product. This is ideal for the guest that wants a big change or something creative and bright.</p>
+                </li>
+            </ul>
+        </div>
+
     </div>
 
     <div class="card">
@@ -69,22 +65,20 @@ get_header();
             <div class="card-top">
                 <h2 class="h2three">Extension</h2>
             </div>
-            <img src="http://placekitten.com/150/150" alt="">
+
         </div>
-        <ul>
-            <li>
-                <p>A consultation is required to ensure color matching and an appropriate maintenance schedule that will fit your lifestyle. </p>
-            </li>
-            <li>
-                <p>A 50% deposit is required to order the extensions once the consultation is complete.</p>
-            </li>
-        </ul>
+        <div class="service-card">
+            <img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/hair-extensions-e1668644796694.jpg" alt="two ladies hugging on sidewalk in the sun.">
+            <ul>
+                <li>
+                    <p>A consultation is required to ensure color matching and an appropriate maintenance schedule that will fit your lifestyle. </p>
+                </li>
+                <li>
+                    <p>A 50% deposit is required to order the extensions once the consultation is complete.</p>
+                </li>
+            </ul>
+        </div>
     </div>
-
-
-
-
-
 </div>
 
 
