1ab2bc9684a8d78e073e70cbeac950c1dff691b9
AzureJames <azurethegreat@gmail.com>
2022-11-19 21:52:45 -0700

    manual merge jehan

--- aboutpage.php
+++ aboutpage.php
@@ -5,21 +5,55 @@ get_header();
 ?>
 
 
-<div class="testing">
-    <h1>ABOUT SAM KWETIO</h1>
+<div class="about-page">
+    <div class="about-img-txt">
+        <div class="about-img first-img">
+            <img src="http://localhost/sams/wp-content/uploads/2022/11/sam-k.webp" alt="Sam-Image">
+        </div><!-- end of about-img-txt -->
 
-    <p>I've been hairstyling for 15 years and have been serving St Albert since 2019.</p>
-    <p>This super fun career has brought me to Montreal, Toronto, California, even Spain, for classes and team events with brands I love working with.</p>
-    <p>Kwetio (Gweh-DEE-oh) is an Iroquois name I've grown up with and have finally embraced.</p>
-    <p>I have two young daughters with my husband, and we're currently commuting in from Morinville.</p>
+            <div class="about-txt">   
+                <div class="about-me ">
+            
+                    <div class="about-heading ">
+                        <h1>ABOUT SAM KWETIO</h1>
+                    </div><!-- end of approach-->
+                    <div class="h1-border"></div>
 
+                    <div class="about-img second-img">
+                        <img src="http://localhost/sams/wp-content/uploads/2022/11/sam-k.webp" alt="Sam-Image">
+                    </div><!-- end of about-img-txt -->
+                    <div class="about-content">
 
-    <div>
-        <h2>My Approach</h2>
-        <p>I'm your service provider first, and your buddy second!</p>
-        <p>This means when you're in my chair. I might say "No" but it's for your benefit, I promise. Getting the hair you want can sometimes mean getting set up for a journey, and I swear you'll look amazing every step of the way. I thrive off of connecting with my guests, so once I get acquainted with your aesthetic, you'll get hairstyles that are authentic to you, AND we'll have a blast! </p>
-    </div>
-</div>
+                        <p>I've been hairstyling for 15 years and have been serving St Albert since 2019.</p>
+                        <p>This super fun career has brought me to Montreal, Toronto, California, even Spain, for classes and team events with brands I love working with.</p>
+                        <p>Kwetio (Gweh-DEE-oh) is an Iroquois name I've grown up with and have finally embraced.</p>
+                        <p>I have two young daughters with my husband, and we're currently commuting in from Morinville.</p>
+                    </div><!-- end of about-content-->
+
+                </div><!-- end of about-me-->
+
+    
+
+            <div class="approach">
+
+                <div class="approach-heading">
+                    <h2>My Approach</h2>
+                </div><!-- end of approach-heading -->
+
+                <div class="approach-content">
+                    <p>I'm your service provider first, and your buddy second!</p>
+                    <p>This means when you're in my chair. I might say "No" but it's for your benefit, I promise. Getting the hair you want can sometimes mean getting set up for a journey, and I swear you'll look amazing every step of the way. I thrive off of connecting with my guests, so once I get acquainted with your aesthetic, you'll get hairstyles that are authentic to you, AND we'll have a blast! </p>
+                    <p>I thrive off of connecting with my guests, so once I get acquainted with your aesthetic, you'll get hairstyles that are authentic to you, AND we'll have a blast!</p>
+                </div>  <!-- end of approach-content -->
+        
+            </div>    <!-- end of approach-->
+            </div><!-- end of about-txt-->
+
+    </div><!-- end of about-txt-->
+        
+
+</div>    <!-- end of about-us -->
+
 
 
 
