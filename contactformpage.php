875ddc7c55ab9eeff2bea9ebc7d05c7700094e18
jehan27 <Jehan_idul@yahoo.ca>
2022-11-19 20:07:35 -0800

    contact-about-page
    css and some div class

--- contactformpage.php
+++ contactformpage.php
@@ -4,9 +4,9 @@
 get_header();
 ?>
 
-<div class="testing">
+<div class="contact-me">
     <h1>CONTACT ME</h1>
-
+    <div class="h1-border"></div>
     <?php echo do_shortcode('[contact-form-7 id="33" title="Contact Me Form"]'); ?>
 </div>
 
--- policiespage.php
+++ policiespage.php
@@ -13,22 +13,18 @@ get_header();
             <div class="card-top">
                 <h2>Policies</h2>
             </div>
-            <div class="card-body">
-                <p>We love all of our clients at Voodoo Hair Studio, and I want to make sure everyone feels at home in our friendly hair salon. That’s why I’ve set a couple basic policies to make sure everyone who walks through our doors has an experience that leaves them wanting to return time and again. Take a quick read. If you have any questions, just get in touch.</p>
-            </div>
+            <p>We love all of our clients at Voodoo Hair Studio, and I want to make sure everyone feels at home in our friendly hair salon. That’s why I’ve set a couple basic policies to make sure everyone who walks through our doors has an experience that leaves them wanting to return time and again. Take a quick read. If you have any questions, just get in touch.</p>
         </div>
 
         <div class="card">
             <div class="card-top">
                 <h2>Cancellations & Rescheduling</h2>
             </div>
-            <div class="card-body">
-                <h3>Just Let Me Know</h3>
-                <p>I know life happens and sometimes things come up, but here are some things to keep in mind when changing your appointments:</p>
-                <p>- Cancellations/Rescheduling with at least 48 hours notice will be moved without issue</p>
-                <p>- Cancellations/Rescheduling within the 48 hour window will require a $50 deposit to book a new appointment.</p>
-                <p>- If you don't show up for your appointment and don't call/email/text at all, the full amount of the missed appointment will be required to book again. A deposit may be required to book future appointments.</p>
-            </div>
+            <p>Just Let Me Know</p>
+            <p>I know life happens and sometimes things come up, but here are some things to keep in mind when changing your appointments:</p>
+            <p>- Cancellations/Rescheduling with at least 48 hours notice will be moved without issue</p>
+            <p>- Cancellations/Rescheduling within the 48 hour window will require a $50 deposit to book a new appointment.</p>
+            <p>- If you don't show up for your appointment and don't call/email/text at all, the full amount of the missed appointment will be required to book again. A deposit may be required to book future appointments.</p>
         </div>
     </div>
 </div>
