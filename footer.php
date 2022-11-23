110f9a0fe4b47cd1a8fd048beca112618c9163cd
jehan27 <Jehan_idul@yahoo.ca>
2022-11-23 10:20:19 -0800

    footer

--- footer.php
+++ footer.php
@@ -15,44 +15,13 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-
-			<p>
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__("Unit 140 "));
-				?>
-			</p>
-
-			<p>
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__("16 Renault Cres. St Albert"));
-				?>
-			</p>
-
-			<p>
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__("samstyres@hotmail.com"));
-				?>
-			</p>
-			<p>
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__("780-470-0124"));
-				?>
-			</p>
-			<ul class="footer-social">
-				<li><a href="#"><img src="https://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/insta.png" alt="instagram logo"></a></li>
-				<li><a href="#"><img src="https://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/fb.png" alt="fb logo"></a></li>
-			</ul>
-			<p>
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__("© Sam Kwetio Hairstyles"));
-				?>
-			</p>
-
+			<div class="upper-footer">
+				<div class="left-side">
+				
+					<p>
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__("Unit 140 16 Renault Cres. St Albert"));
 						?>
 					</p>
 
@@ -71,30 +40,24 @@
 				</div>
 				<div class="right-side">
 					<ul class="footer-social">
-
 						
 					
 						 <li class="instagram"><a href="#"><img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/insta.png" alt="instagram logo"></a></li>
 
 						<li class="facebook"><a href="#"><img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/fb.png" alt="fb logo"></a></li> 
-
 					</ul>
 				</div>
 			</div>
 
-
 			<!-- Â-->
-
 			<div class="copyright">
 				<p>
 					<?php
 					/* translators: %s: CMS name, i.e. WordPress. */
-
 					printf( esc_html__("© Sam Kwetio Hairstyles"));
 					?>
 				</p>
 			</div>
-
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
