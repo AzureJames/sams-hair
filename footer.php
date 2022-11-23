cc45bf80d8a8e4a3c3562f9701512c886a43f730
jehan27 <Jehan_idul@yahoo.ca>
2022-11-22 18:51:16 -0800

    footer-contact-form

--- footer.php
+++ footer.php
@@ -15,6 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
+<<<<<<< Updated upstream
 			<p>
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
@@ -51,6 +52,51 @@
 				printf( esc_html__("© Sam Kwetio Hairstyles"));
 				?>
 			</p>
+=======
+			<div class="upper-footer">
+				<div class="left-side">
+				
+					<p>
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__("Unit 140 16 Renault Cres. St Albert"));
+						?>
+					</p>
+
+					<p>
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__("samstyres@hotmail.com"));
+						?>
+					</p>
+					<p>
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__("780-470-0124"));
+						?>
+					</p>
+				</div>
+				<div class="right-side">
+					<ul class="footer-social">
+						
+					
+						 <li class="instagram"><a href="#"><img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/insta.png" alt="instagram logo"></a></li>
+
+						<li class="facebook"><a href="#"><img src="http://samlebel.web.dmitcapstone.ca/wp-content/uploads/2022/11/fb.png" alt="fb logo"></a></li> 
+					</ul>
+				</div>
+			</div>
+
+			<!-- Â-->
+			<div class="copyright">
+				<p>
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__("© Sam Kwetio Hairstyles"));
+					?>
+				</p>
+			</div>
+>>>>>>> Stashed changes
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
