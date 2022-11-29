e9a2cabd6046194d1a2b9660ac144bf2f4fb38ab
Justin Shaw <shaw.justin@shaw.ca>
2022-11-29 14:47:04 -0700

    Fixed Site

--- contactformpage.php
+++ contactformpage.php
@@ -5,9 +5,14 @@ get_header();
 ?>
 
 <div class="contact-me">
-    <h1>CONTACT ME</h1>
-    <div class="h1-border"></div>
-    <?php echo do_shortcode('[contact-form-7 id="33" title="Contact Me Form"]'); ?>
+    <div class="contact-page">
+        <div class="header-bar">
+            <h1>CONTACT ME</h1>
+            <div class="h1-border"></div>
+        </div>
+        <?php echo do_shortcode('[contact-form-7 id="33" title="Contact Me Form"]'); ?>
+    </div>
+
 </div>
 
 
